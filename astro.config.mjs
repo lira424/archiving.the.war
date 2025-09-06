12b2a5e3ef827f2735bd7fdbd85ef61fd88a37c4
Kavehrafie <kavehrafie@gmail.com>
2025-09-05 23:07:04 -0700

    - Created Head.astro component to manage document head elements and font loading.
    - Added example.mdx documentation showcasing usage of Popover and LinkCard components.
    - Introduced custom.css for global styles, currently empty.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+    experimental:{
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Vazirmatn",
+            cssVariable: "--font-vazirmatn",
+        }]
+    },
   integrations: [
       starlight({
           title: 'My Docs',
+          customCss: ['./styles/custom.css'],
+          components: {
+            Head: './src/components/Head.astro'
+          },
           social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
           sidebar: [
               {
